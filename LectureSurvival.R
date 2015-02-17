25719d3eff0388ec442f773f5d25d8bf1acd1ade
unknown <petstr@D0156MEB.meb.ki.se>
2015-02-17 12:51:12 +0100

    some more

--- LectureSurvival.R
+++ LectureSurvival.R
@@ -1,6 +1,14 @@
-## ----P1 --------------
-ls(pos = "package:bmBiostat")
-data(package = bmBiostat)
+## ----HIVdata --------------
+# ls(pos = "package:bmBiostat")
+# data(package = bmBiostat)
+
+
+## ----HIVdata --------------
+hiv <- read.table(
+  "http://www.ats.ucla.edu/stat/R/examples/asa/hmohiv.csv",
+  sep=",", header = TRUE)
+
+head(hiv)
 
 ## ----P2
 
@@ -9,3 +17,11 @@ b
 data(cars)
 cars
 a<-lm(speed~dist, cars)
+
+## ----Lexis
+hiv_L <- cal.yr( hiv, format="%m/%d/%Y", wh=6:7 )
+
+Lcoh <- Lexis( entry = list( per=as.numeric(entdate) ),
+               exit = list( per=as.numeric(enddate)),
+               exit.status = censor,
+               data = hiv )
