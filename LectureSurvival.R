ea7e44f04dc55f2c30cb7242120ab386cd154e64
unknown <petstr@D0156MEB.meb.ki.se>
2015-04-07 16:54:47 +0200

    apr 7 nr2

--- LectureSurvival.R
+++ LectureSurvival.R
@@ -51,10 +51,10 @@ Lex2 <- Lexis( duration = list( months=time),
 
 ## ----Censor
 plot(Lex2[1:2, ], type="b",pch=c(NA,16)[Lex2$censor+1], ylim=c(0, 3), xlim=c(0, 10), 
-     ylab="Subject", xlab="Time", yaxt="n", xaxt="n", main="Visualizing survival data")
+     ylab="Subject", xlab="Time", yaxt="n", xaxt="n", main="Visualizing survival data", lwd=2)
 
 ## ----Lexis0
-plot(Lex2, type="b",  pch=c(NA,16)[Lex2$censor+1], col=c("red", "blue")[Lex2$drug+1])
+plot(Lex2, type="b",  pch=c(NA,16)[Lex2$censor+1], col=c("red", "blue")[Lex2$drug+1], lwd=2)
 abline(v=sort(Lex2[Lex2$censor==1,]$enddate - Lex2[Lex2$censor==1,]$entdate)[1])
 abline(v=sort(Lex2[Lex2$censor==1,]$enddate - Lex2[Lex2$censor==1,]$entdate)[2])
 abline(v=sort(Lex2[Lex2$censor==1,]$enddate - Lex2[Lex2$censor==1,]$entdate)[3])
@@ -76,18 +76,18 @@ abline(v=sort(Lex2[Lex2$censor==1,]$enddate - Lex2[Lex2$censor==1,]$entdate))
 # abline(v=sort(Lex[Lex$censor==1,]$enddate)[4])
 
 ## ----Surv1
-plot(Lex1, type="b", pch=c(NA,16)[Lex1$censor+1], xlab="t (months)", ylab="")
+plot(Lex1, type="b", pch=c(NA,16)[Lex1$censor+1], xlab="t (months)", ylab="", lwd=2, cex.lab=1.5)
 abline(v=5)
 
 ## ----Surv2
 Lex1$lex.dur[Lex1$time >= 8 ] <- 8
 Lex1$censor[Lex1$time >= 8 ] <- 0
-plot(Lex1, type="b", pch=c(NA,16)[Lex1$censor+1], xlab="t (months)", ylab="")
+plot(Lex1, type="b", pch=c(NA,16)[Lex1$censor+1], xlab="t (months)", ylab="", lwd=2, cex.lab=1.5)
 abline(v=8, lty=2)
 abline(v=5)
 
 ## ----Surv3
-plot(Lex2, type="b", pch=c(NA,16)[Lex2$censor+1], xlab="t (months)", ylab="")
+plot(Lex2, type="b", pch=c(NA,16)[Lex2$censor+1], xlab="t (months)", ylab="", lwd=2, cex.lab=1.5)
 abline(v=5)
 
 ## ----Surv4
@@ -102,14 +102,15 @@ plot(survfit(Surv(time=time, event=censor) ~ drug, data=hiv), col=c("red", "blue
 survdiff(Surv(time=time, event=censor) ~ drug, data=hiv)
 
 ## ----plotHIV
-plot(Lex2, type="b",  pch=c(NA,16)[Lex2$censor+1], col=c("red", "blue")[Lex2$drug+1], xlab="")
+plot(Lex2, type="b",  pch=c(NA,16)[Lex2$censor+1], col=c("red", "blue")[Lex2$drug+1], xlab="",
+     lwd=2, cex.lab=1.5)
 
 ## ----plotChisq
 curve( dchisq(x, df=1), col='red', main = "Chi-Square Density Graph",
-       from=0,to=6, ylab="")
-abline(v=0.1667)
-text(x=0.6,y=1.5,"p=0.8")
-abline(v=qchisq(.95, df=1))
+       from=0,to=6, ylab="", lwd=2, cex.lab=1.5)
+abline(v=0.1667, lwd=2)
+text(x=0.8, y=1.5, "p=0.8", cex=2)
+abline(v=qchisq(.95, df=1), lwd=2)
 
 ## ----LogRankAgeCat
 hiv$agecat <- cut(hiv$age, c(min(hiv$age), 29, 34, 39, 
@@ -124,9 +125,11 @@ coxph(Surv(time=time, event=censor) ~ agecat, data=hiv)
 hiv$drug <- as.factor(hiv$drug)
 coxph(Surv(time=time, event=censor) ~ drug + age, data=hiv)
 
-## ----Cox3
+## ----Cox3a
 cox <- coxph(Surv(time=time, event=censor) ~ agecat + drug, data=hiv)
 predict <- data.frame(drug=c(0,1), agecat=rep(levels(hiv$agecat)[1], 2))
+
+## ----Cox3b
 plot(survfit(cox, newdata=predict), col=c("red","blue"))
 legend("topright", legend=c('drug = 0', 'drug = 1'), lty=c(1,1), col=c("red","blue"))   
 
@@ -143,12 +146,16 @@ model1 <- coxph(Surv(time=time, event=censor) ~ drug, data=hiv)
 model2 <- coxph(Surv(time=time, event=censor) ~ agecat + drug, data=hiv)
 anova(model1, model2)
 
-## ----PropHaz1
+## ----PropHaz1a
+cox <- coxph(Surv(time=time, event=censor) ~ drug + age, data=hiv)
+plot(cox.zph(cox))
+
+## ----PropHaz1b
 cox <- coxph(Surv(time=time, event=censor) ~ drug + age, data=hiv)
 par(mfrow=c(1,2))
 plot(cox.zph(cox))
 
-## ----ParMfrow
+## ----ParMfrow2
 par(mfrow=c(1,1))
 
 ## ----PropHaz2
